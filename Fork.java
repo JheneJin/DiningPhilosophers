c374fdd00235e3a2fc1587c85644344d186cb6d8
JheneJin <80924562+JheneJin@users.noreply.github.com>
2023-10-15 19:49:20 -0700

    Update Fork.java

--- Fork.java
+++ Fork.java
@@ -17,31 +17,38 @@ public class Fork {
         this.lock = new ReentrantLock();
         //making condion connected to lock (MIGHT BE WRONG)
         this.condition = lock.newCondition();
-
+        //boolean to flag if the fork is in use or not in use
         this.inUse = false;
     }
 
     public void takeFork(int philNumber) {
+        //locks the fork
         lock.lock();
         try {
+            //display the fork id and who is using it
             System.out.println("Fork #" + id + " is being used by Philosopher #" + philNumber);
             while (inUse == true) {
+                //philosopher waits for the fork to not be in use
                 condition.await(); 
             }
             inUse = true;
         } catch (InterruptedException e) {
             System.out.println(e);
         } finally {
+            //after the philosopher is done using the fork, it will unlock the fork
             lock.unlock();
         }
     }
     
     public void returnFork() {
+        //locks the fork
         lock.lock();
         try {
+            //fork is not in use, so signals to the philospher can freely take it
             inUse = false;
             condition.signal();
         } finally {
+             //after the philosopher is done using the fork, it will unlock the fork
             lock.unlock();
         }
     }
